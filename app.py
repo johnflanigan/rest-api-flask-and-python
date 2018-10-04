8ebc1cd45c27fd7592ded44603e8c80f608af228
John Flanigan <johnrflanigan@gmail.com>
2018-10-04 08:42:45 -0400

    Updated app.py with outline of API

--- app.py
+++ app.py
@@ -2,10 +2,55 @@ from flask import Flask
 
 app = Flask(__name__)
 
+stores = [
+    {
+        'name': 'My Wonderful Store',
+        'items': [
+            {
+                'name': 'My Item',
+                'price': 15.99
+            }
+        ]
+    }
+]
 
-@app.route('/')  # 'http://www.google.com/'
-def home():
-    return "Hello, world!"
+# POST - used to receive data
+# GET - used to send data back only
+
+
+# POST /store data: {name:}
+@app.route('/store', methods=['POST'])
+def create_store():
+    pass
+
+
+# GET /store/<string:name>
+@app.route('/store/<string:name>')  # 'http://127.0.0.1:5000/store/some_name'
+def get_store(name):
+    pass
+
+
+# GET /store
+@app.route('/store')
+def get_stores():
+    names = [store['name'] for store in stores]
+
+    string = ''
+    for name in names:
+        string = string + name
+    return string
+
+
+# POST /store/<string:name>/item {name:, price:}
+@app.route('/store/<string:name>/item', methods=['POST'])
+def create_item_in_store(item):
+    pass
+
+
+# GET /store/<string:name>/item
+@app.route('/store/<string:name>/item')
+def get_item_in_store(item):
+    pass
 
 
 app.run(port=5000)
